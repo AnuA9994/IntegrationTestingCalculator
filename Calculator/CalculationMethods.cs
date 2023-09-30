1788ed2e181232fe64390df5b191a551cf58a389
anuaj <anuaj@ANUAJITH>
2023-09-29 20:55:02 -0400

    unit tests added

--- Calculator/CalculationMethods.cs
+++ Calculator/CalculationMethods.cs
@@ -20,8 +20,9 @@ namespace Calculator
         public static int divide(int num1, int num2)
         {
             if (num2 == 0)
-                throw new("Cannot divide by zero");
+                throw new ArithmeticException("Divide By Zero Error");
             return num1 / num2;
+            
         }
     }
 }
---
+++ IntegrationTestingCalculator/integration_test.cs
@@ -0,0 +1,45 @@
+using Calculator;
+using IntegrationTestingCalculator;
+namespace IntegrationTestingCalculator
+{
+    public class integration_test
+    {
+        [Test]
+        public void Addition()
+        {
+            int num1 = 5;
+            int num2 = 10;
+           Assert.AreEqual(15,CalculationMethods.Add(num1,num2));
+        }
+        [Test]
+        public void Subtraction()
+        {
+            int num1 = 25;
+            int num2 = 3;
+            Assert.AreEqual(22, CalculationMethods.Subtract(num1, num2));
+        }
+        [Test]
+        public void  Multiplication()
+        {
+            int num1 = 3;
+            int num2 = 6;
+            Assert.AreEqual(18, CalculationMethods.multiply(num1, num2));
+        }
+        [Test]
+        public void DivisionPositive()
+        {
+            int num1 = 9;
+            int num2 = 3;
+            Assert.AreEqual(3, CalculationMethods.divide(num1, num2));
+        }
+        [Test]
+        public void DivisionNegative()
+        {
+            int num1 = 9;
+            int num2 = 0;
+            string expectedErrorMessage = "Divide By Zero Error";
+            var ex = Assert.Throws<ArithmeticException>(() => CalculationMethods.divide(num1, num2));
+            Assert.AreEqual(expectedErrorMessage, ex.Message);
+        }
+    }
+}
